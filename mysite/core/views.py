2328187aeee64ea787c580b4d08a20757d14b411
Leonardo Lopes <73510665+CodamaisEJ@users.noreply.github.com>
2022-03-30 22:05:37 -0300

    arrumando bugs

---
+++ mysite/core/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, CreateView
+from django.core.files.storage import FileSystemStorage
+from django.urls import reverse_lazy
+
+import pandas as pd
+
+from .forms import BookForm
+from .models import Book
+
+pr = pd.read_csv(
+    'media/Pasta1.csv',
+    encoding='utf8',
+    sep=';'
+)
+print(pr.head())
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+
+def upload(request):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+
+    return render(request, 'upload.html', context)
+
+
+
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {
+        'books': books
+    })
+
+
+def upload_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'upload_book.html', {
+        'form': form
+    })
+
+
+def delete_book(request, pk):
+    if request.method == 'POST':
+        book = Book.objects.get(pk=pk)
+        book.delete()
+    return redirect('book_list')
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'class_book_list.html'
+    context_object_name = 'books'
+
+
+
+class UploadBookView(CreateView):
+    model = Book
+    form_class = BookForm
+    success_url = reverse_lazy('class_book_list')
+    template_name = 'upload_book.html'
+
+
+
