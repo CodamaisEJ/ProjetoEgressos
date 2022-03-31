5d2fca95441927baab025f60c4795bdab70d0f97
Leonardo Lopes <73510665+CodamaisEJ@users.noreply.github.com>
2022-03-30 22:00:13 -0300

    Adiconando arquivos

---
+++ my_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/leona/Desktop/CodaMais/Projeto de engressos/django-upload-example-master/my_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
